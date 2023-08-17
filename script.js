6fc2dfa8137b77b20305cd85faef20545f403397
yulia19882019 <uliapuskova5@gmail.com>
2023-08-17 16:17:34 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+//alert
+//prompt
+//cofirm
+
+//if(условие)
+//let num = 5
+
+//let answer = prompt("Угадай число?")
+//if (answer == num) { alert("Поздравляю") }
+//else { alert("Не угадал") }
+//function название_функции( аргумент 1, аргумент 2) 
+
+
+//Let, const - создания переменных
+//Number, Sting, BooLean, null, undefiend, NaN - типы данных
+// if() - условные операторы
+//while, do while, for - циклы
+//break, continue - прервать цикл
+//function - функции
+
+
+//function yrovnenie(a, b, c){
+//    const discriminant=b*b-4*a*c;
+//    if(discriminant==0){
+//        return-b/(2*a);
+ //   } else if(discriminant >0){
+  //      let x1=(-b+Math.sqrt(discriminant))/(2*a);
+ //       let x2=(-b+Math.sqrt(discriminant))/(2*a); 
+  //  }
+//}
+
+//function sum(a,b){
+//  return a+b;
+//}
+//const return = sum=(2,7);
+//document.write(res
